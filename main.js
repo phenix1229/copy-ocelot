971a331422a32f5c9b4c9ae2356614834f20f922
phenix1229 <paul.garay@codeimmersives.com>
2019-11-06 12:39:47 -0500

    finished section 3

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+window.onscroll = function(){changeNav()}
 document.querySelector('#os-button').addEventListener('mouseover', invertColorWhite)
 document.querySelector('#os-button').addEventListener('mouseout', invertColorBlack)
-document.addEventListener('scroll', function(){document.querySelector('#nav').style.backgroundColor = 'white'})
 document.querySelector('#s1l-bottom').addEventListener('mouseout', invertColorWhite)
 document.querySelector('#s1l-bottom').addEventListener('mouseover', invertColorBlack)
 document.querySelector('#s2l-bottom').addEventListener('mouseout', invertColorWhite)
 document.querySelector('#s2l-bottom').addEventListener('mouseover', invertColorBlack)
 document.querySelector('#s2r-bottom').addEventListener('mouseout', invertColorWhite)
 document.querySelector('#s2r-bottom').addEventListener('mouseover', invertColorBlack)
+document.querySelector('#classic-btn').addEventListener('mouseout', invertColorWhite)
+document.querySelector('#classic-btn').addEventListener('mouseover', invertColorBlack)
 
+
+function changeNav(){
+    if(document.body.scrollTop >20  || document.documentElement.scrollTop >20){
+        document.querySelector('#nav').style.backgroundColor = 'white'
+    } else {
+        document.querySelector('#nav').style.backgroundColor = ''
+    }
+}
 function invertColorBlack(){event.target.style.backgroundColor = 'black'; event.target.style.color = 'white'}
 function invertColorWhite(){event.target.style.backgroundColor = 'white'; event.target.style.color = 'black'}
