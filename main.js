3337e3681def64b551a9b186f338263d0fbc9b3d
phenix1229 <paul.garay@codeimmersives.com>
2019-11-06 11:54:45 -0500

    finished section 2

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ document.querySelector('#os-button').addEventListener('mouseout', invertColorBla
 document.addEventListener('scroll', function(){document.querySelector('#nav').style.backgroundColor = 'white'})
 document.querySelector('#s1l-bottom').addEventListener('mouseout', invertColorWhite)
 document.querySelector('#s1l-bottom').addEventListener('mouseover', invertColorBlack)
+document.querySelector('#s2l-bottom').addEventListener('mouseout', invertColorWhite)
+document.querySelector('#s2l-bottom').addEventListener('mouseover', invertColorBlack)
+document.querySelector('#s2r-bottom').addEventListener('mouseout', invertColorWhite)
+document.querySelector('#s2r-bottom').addEventListener('mouseover', invertColorBlack)
 
 function invertColorBlack(){event.target.style.backgroundColor = 'black'; event.target.style.color = 'white'}
 function invertColorWhite(){event.target.style.backgroundColor = 'white'; event.target.style.color = 'black'}
