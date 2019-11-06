067b042257a4b509e759010e221037d9496af7ca
phenix1229 <paul.garay@codeimmersives.com>
2019-11-05 22:03:52 -0500

    almost finished section 1

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 document.querySelector('#os-button').addEventListener('mouseover', function(){event.target.style.backgroundColor = 'white'; event.target.style.color = 'black'})
 document.querySelector('#os-button').addEventListener('mouseout', function(){event.target.style.backgroundColor = 'black'; event.target.style.color = 'white'})
+document.addEventListener('scroll', function(){if(document.scrolltop = 0){document.querySelector('#nav').style.backgroundColor = 'none'} else {document.querySelector('#nav').style.backgroundColor = 'white'}})
+
+
+
+
